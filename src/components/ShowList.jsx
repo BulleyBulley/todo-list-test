648674e22c67e5bc0ecda92809513668bf85d0d8
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-10-09 20:07:01 +0100

    adds move to completed

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,29 +1,36 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function ShowList(props) {
   const { list } = props;
-  const { setList } = props
-  const [newListItem, setNewListItem] = useState();
+  const { setList } = props;
+  
 
   function handleDelete(task) {
-    const newList = list.filter((item) => item.task !== task)
+    const newList = list.filter((item) => item.task !== task);
+    setList(newList);
+  }
 
-    setList(newList)
+  function handleCompleted(task) {
+    const newList = list.filter((item) => item.completed = true);
+    setList(newList);
   }
 
   
+
   return (
     <ul>
       {list.map((item) => {
-          if (item.completed === false) {
-        return (
-          <li key={item.task}>
-            <button type="button" onClick={() => handleDelete(item.task)} >Remove Task</button>
-            {item.task}
-            <button type="button" >Move to Completed</button>
-          </li>
-        );
-          }
+        if (item.completed === false) {
+          return (
+            <li key={item.task}>
+              <button type="button" onClick={() => handleDelete(item.task)}>
+                Remove Task
+              </button>
+              {item.task}
+              <button type="button" onClick={() => handleCompleted(item.task)}>Move to Completed</button>
+            </li>
+          );
+        }
       })}
     </ul>
   );
