8e4ddebd440e7db7d57c140e9900449577f6c4d3
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-10-10 12:39:37 +0100

    adds drag and drop to ToDo list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-
+import { DragDropContext } from 'react-beautiful-dnd';
 import Header from './components/Header.jsx';
 import ListAdd from './components/ListAdd.jsx';
 import ShowList from './components/ShowList.jsx';
@@ -13,6 +13,7 @@ function App() {
   const [list, setList] = useState([])
   
   
+  
   return (
     <div className="App">
 
@@ -20,6 +21,7 @@ function App() {
       <ListAdd setList={setList} />
       <ShowList list={list} setList={setList}/>
       <ShowCompletedList list={list} setList={setList} />
+      
 
         
     </div>
--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 function ShowList(props) {
   const { list } = props;
   const { setList } = props;
+
+  <DragDropContext onDragEnd={handleOnDragEnd}></DragDropContext>;
 
   function handleDelete(task) {
     const newList = list.filter((item) => item.task !== task);
@@ -9,37 +14,68 @@ function ShowList(props) {
 
   const updateStatus = (task, newStatus) => {
     const newList = list.map((item) => {
-        if (item.task === task) {
-            item.completed = newStatus
-        }
-        return item
-    })
-    setList(newList)
+      if (item.task === task) {
+        item.completed = newStatus;
+      }
+      return item;
+    });
+    setList(newList);
+  };
+
+  function handleOnDragEnd(result) {
+    const items = Array.from(list);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setList(items);
   }
-  
+
   return (
-      <section className="show_list_class">
-        <h2>ToDo:</h2>
-    <ul>
-      {list.map((item) => {
-        if (item.completed === false) {
-          return (
-            <li key={item.task}>
-              <button type="button" className="list_button" onClick={() => handleDelete(item.task)}>
-                Remove
-              </button>
-              {item.task}
-              <button
-                type="button"
-                onClick={() => updateStatus(item.task, true)}
-              >
-                Completed
-              </button>
-            </li>
-          );
-        }
-      })}
-    </ul>
+    <section className="show_list_class">
+      <h2>ToDo:</h2>
+      
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="list">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {list.map((item, index) => {
+                if (item.completed === false) {
+                  return (
+                    <Draggable
+                      key={item.task}
+                      draggableId={item.task}
+                      index={index}
+                    >
+                      {(provided) => (
+                        <li
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                        >
+                          <button
+                            type="button"
+                            className="list_button"
+                            onClick={() => handleDelete(item.task)}
+                          >
+                            Remove
+                          </button>
+                          {item.task}
+                          <button
+                            type="button"
+                            onClick={() => updateStatus(item.task, true)}
+                          >
+                            Completed
+                          </button>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                }
+              })}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </section>
   );
 }
